5ae64492cbf88ed3fe8c36f7a028b0e4c36ef464
Gezsh <teramedewgezhagn@gmail.com>
2023-10-23 14:22:02 +0300

    update model

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,6 +1,19 @@
-const login = async (req, res) => {};
+const User=require('../models/user')
 
-const register = async (req, res) => {};
+const login = async (req, res) => {
+     const {email, password} = req.body
+     if(!email || !password){
+
+     }
+     
+};
+
+const register = async (req, res) => {
+    
+     const user=await User.create({...req.body})
+     const token=user.createJWT
+     res.status(200).json({user:user,token})
+};
 
 module.exports = {
   login,
