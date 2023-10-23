7d82bd16af498576a92f28ea25d2f6a50b5fc658
abela2112 <abela9326@gmail.com>
2023-10-23 14:18:58 +0300

    add error handler middleware

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,24 @@
 const express =require('express')
 const cors = require("cors");
+require("express-async-errors");
 const app = express();
 require("dotenv").config();
 //connect database
 const dbconnect = require("./connect/db");
-
+//middleware
+const errorHandleMiddleware = require("./middleware/errorhandler");
+const notFoundErrorMiddleware = require("./middleware/notFound");
 //router
 const userRoute = require("./routes/user");
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoute);
+
+// error handler
+app.use(errorHandleMiddleware);
+app.use(notFoundErrorMiddleware);
+
+
 app.listen(process.env.PORT || 5000 ,async()=>{
     await dbconnect(process.env.MONGO_URL)
     console.log('Server is listening')
--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -1,4 +1,12 @@
-const login = async (req, res) => {};
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+  }
+};
 
 const register = async (req, res) => {};
 
