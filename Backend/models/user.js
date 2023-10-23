1457d9d28284d984bca82ccc8051ee035d92b733
Gezsh <teramedewgezhagn@gmail.com>
2023-10-23 13:42:15 +0300

    update user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,7 +1,12 @@
 const mongoose=require('mongoose')
 
 const UserSchema =new mongoose.Schema({
-    name:{
+    First_name:{
+        type:String,
+        require:[true,'name must be provided'],
+
+    },
+    Last_name:{
         type:String,
         require:[true,'name must be provided'],
 
@@ -9,9 +14,36 @@ const UserSchema =new mongoose.Schema({
     email:{
         type:String,
         required:[true,'email must be provided'],
-        unique:true,
-        match:
+       
+        match:[
+            /^[\w.-]+@MinT\.gov\.et$/,
+           'please provide valid email'
+
+        ],
+        unique:true
+
+       },
+  position:{
+      type:String,
+      required:[true,'position must be provided'],
+      enum:['CEO','DESK','EXPERT'],
+      default:'EXPERT'
+     },
+  password:{
+        type:String,
+         require:[true,'password must be provided'],
+         
+    } ,
+    department:{
+        type:String,
+        require:true,
+
+    },
+    phoneNumber:{
+        type:String,
+        require:true
     }
-})
+
+  })
 
 module.exports=mongoose.model('User',UserSchema)
